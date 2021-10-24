745feaac3422d23988821a04886d1dc8553ab8dc
Kiyomov Abdulloh <qiyomovabdulloh3@gmail.com>
2021-10-24 08:34:22 +0500

    start

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,20 @@
-console.log(`Takrorlash`);
+//Data types on Javascript
+//1)Primitive 
+//~Numbers
+//~Strings
+//~Null
+//~Boolean
+//~Undefined
+
+//2)NonPrimitive
+//~Object
+//~Function
+//~Array
+
+let yosh = 16
+
+if (yosh === 17) {
+    console.log(`Ukang kuchaydi uje`);
+}else{
+    console.log(`Uka damata gir qitekak`);
+}
